76c68156196b53a2b080b5ee0b1a7da730b870e1
Gareth Carew <50182665+GarethCarew@users.noreply.github.com>
2024-06-22 15:16:36 -0500

    Finished?

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const express = require("express")
-const bodyParser = require("body-parser")
-const request = require("request")
-const https = require("https")
+const bodyParser = require("body-parser");
+const { it } = require("node:test");
+
 const app = express()
-app.use(express.static("public"))
+
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+app.set('view engine', 'ejs');
+
+let items = []
+let workItems = []
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/signup.html")
+
+    let today = new Date()
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    let day = today.toLocaleString("en-US", options)
+
+    res.render("list", {listTitle: day, newListItems: items})
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.get("/work", (req, res) => {
+
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+app.post("/", (req, res) => {
+
+    item = req.body.newItem
+    if(req.body.list === "Work List"){
+        workItems.push(item)
+        res.redirect("/work")
+    } else {
+        items.push(item)
+        res.redirect("/")
+    }
+})
+
+app.listen(3000, () => {
     console.log("server is running")
 })
